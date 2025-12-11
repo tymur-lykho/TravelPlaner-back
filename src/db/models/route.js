c5aa342730c82e32904ed94bc348391dd166c565
Tymur Lykho <tima.likho@gmail.com>
2025-12-11 15:29:03 +0200

    update: route model

--- src/db/models/route.js
+++ src/db/models/route.js
@@ -15,23 +15,48 @@ export const routeSchema = new Schema(
         ref: 'Comment',
       },
     ],
-    points: [
+    steps: [
       {
+        type: {
+          type: String,
+          enum: ['reference', 'custom'],
+          required: true,
+        },
+
         order: {
           type: Number,
           required: true,
         },
+
         point: {
           type: Schema.Types.ObjectId,
           ref: 'Point',
+        },
+
+        customData: {
+          name: String,
+          description: String,
+          lngLat: {
+            type: { type: String, enum: ['Point'] },
+            coordinates: { type: [Number] },
+          },
         },
       },
     ],
     time: {
       type: Number,
     },
+    length: {
+      type: Number,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
   { timestamps: true, versionKey: false },
 );
+
+routeSchema.index({ lngLat: '2dsphere' });
 
 export const RoutesCollection = model('Route', routeSchema);
